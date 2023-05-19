0eb1c6cb1a6321f8f713c768cc5f159bffe09245
Dalaxshan <ujanth98@gmail.com>
2023-05-20 02:48:42 +0530

    add more component paths

--- Frontend/frontend/src/App.js
+++ Frontend/frontend/src/App.js
@@ -1,10 +1,15 @@
-import logo from "./logo.svg";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import Home from "./pages/Home";
-import Header from "./pages/Header";
-import Footer from "./pages/Footer";
-import ViewProduct from "./pages/ViewProduct";
+import Home from "./Pages/Home";
+import Header from "./Pages/Header";
+import Footer from "./Pages/Footer";
+import ViewProduct from "./Pages/ViewProduct";
+import Register from "./Pages/Register";
+import EditInformation from "./Pages/EditInformation";
+import EmployeesList from "./Pages/EmployeesList";
+import InformerHome from "./Pages/InformerHome";
+import CreateInformation from "./Pages/CreateInformation";
+import EditProfile from "./Pages/EditProfile";
 
 function App() {
   return (
@@ -13,7 +18,14 @@ function App() {
       <Header />
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/informer" element={< InformerHome />}/>
+          <Route exact path="/employeesList" element={<EmployeesList/>} />
           <Route exact path="/viewProduct" element={<ViewProduct />} />
+          <Route exact path="/CreateInformation" element={<CreateInformation/>}/>
+          <Route exact path="/editInformation/:id" element={<EditInformation/>}/>
+          <Route exact path="employeesList/editProfile/:id" element={<EditProfile/>}/>
+
         </Routes>
         <Footer />
       </Router>
