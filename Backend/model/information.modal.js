b9829802f016dd72f895a73cf90d93d4920283d5
Dalaxshan <ujanth98@gmail.com>
2023-05-20 02:46:55 +0530

    Change information schema' column

--- Backend/model/information.modal.js
+++ Backend/model/information.modal.js
@@ -3,12 +3,12 @@ const schema = mongoose.Schema;
 
 //create schema
 const Information_schema = new schema({
-  first_Name: { type: String, required: true,unique:true,min:3,max:20},
-  last_Name: { type: String, required: true,min:3,max:20},
+  first_Name: { type: String, required:true},
+  last_Name: { type: String, required: true},
   address: { type: String, required: true},
   contact_no: { type: Number, required: true},
-  qty: { type: Number, required: true},
-  description:{type:String, required: true,min:3,max:50}
+  informer_Name: { type: String, required: true},
+  description:{type:String, required: true}
 });
 
 const information=mongoose.model('information',Information_schema);
