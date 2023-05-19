40d2da67228fa14f913e8caf04a99483676faadb
Dalaxshan <ujanth98@gmail.com>
2023-05-20 02:55:02 +0530

    Set mongoDB ACL header and Methods

--- Backend/index.js
+++ Backend/index.js
@@ -6,6 +6,12 @@ const cors=require('cors');
 const app=express();
 require('dotenv').config();
 
+app.use((req, res, next) => {
+   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+   next();
+ });
 
 app.use(express.json());
 
